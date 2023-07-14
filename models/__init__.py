67de8ff3ca04e264e56e6ba71563f9eb370f6087
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-07-14 06:39:11 -0700

    add documetation to module

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,7 @@
+""" package module that entry point of package
+    contain storage variable that represent unique storage
+    for project
+"""
 from .engine.file_storage import FileStorage
 
 storage = FileStorage()
