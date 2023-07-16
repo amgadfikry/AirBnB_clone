bf6d2ec6e0ee96af4b9b2da5bc2393c0fa9bd129
abubakrali10 <bakrali175@yahoo.com>
2023-07-17 02:16:48 +0300

    fixed type

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ package module that entry point of package
-    contain storage variable that represent unique storage
+    contains storage variable that represent unique storage
     for project
 """
 from models.engine.file_storage import FileStorage
